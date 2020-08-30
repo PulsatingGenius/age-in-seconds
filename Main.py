528dd03834f1af7885ec0defeb6f605ad19f3a78
Aman Anand <69569434+PulsatingGenius@users.noreply.github.com>
2020-08-30 22:34:46 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,36 @@
+print (" hello world. ".title())
+print (" what is your name. ?".title())
+name = input()
+print (" wow thats very nice name 😍😍😍 . ".title () + name.upper() )
+print (" and its a pleasure to meet you 🤗🤗 . ".title() + name.upper() )
+print (" I am going to ask you some questions .".title())
+# I am trying to build a program that will calculate there age by every possible way
+print (" I hope your are ready !!! .".title())
+print (" what is your age ? ".title())
+age = input() # Here whoever is the user will input his age and that age will multiply by 
+              # those numbers which is 12 which is for months 
+              # 31 is for days in that months 
+              # 24 is for the hours in those days 
+              # 60 is for the minutes in those hours 
+              # and again 60 is for minutes 
+              # I HOPE YOU WILL LIKE IT 
+              
+              # PLESE GIVE ME A STAR , FOR SHOWING YOUR LOVE ☺☺☺
+
+print (" according to your age ::".title ())
+print (" you are ".title() + str(int(age) *12 ) + (" months old. 😁😁. ".title()))
+
+print(" you are ".title() + str(int(age) *12*31) + (" days old 😉😉. ".title()))
+
+print(" you are ".title() + str(int(age) *12*31*24) + (" hours old 🤐🤐 .".title()))
+
+print(" you are ".title() + str(int(age) *12*31*24*60) + (" minutes old 😲😲. ".title()))
+
+print(" you are ".title() + str(int(age)*12*31*24*60*60) + (" seconds old 😱😱😱. ".title()))
+
+
+print ("how awesome it is when you got to know how many seconds you old are . ".title())
+print(" bye , and have a good day ☺ .".title())
+        
+       
+       
